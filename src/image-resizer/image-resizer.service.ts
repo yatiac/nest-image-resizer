5f167686115c7daf08e0436edddc3a5c100d019d
Rafael Echeverria (yatiac) <master.152@gmail.com>
2021-03-11 23:04:57 -0500

    cast string tu number

--- src/image-resizer/image-resizer.service.ts
+++ src/image-resizer/image-resizer.service.ts
@@ -10,8 +10,8 @@ export class ImageResizerService {
   ): Promise<Buffer> {
     const sharpImage = sharp(fileBuffer);
     const imageMetadata = await sharpImage.metadata();
-    height = height ? height : imageMetadata.height / 2;
-    width = width ? width : imageMetadata.width / 2;
+    height = height ? +height : imageMetadata.height / 2;
+    width = width ? +width : imageMetadata.width / 2;
     console.log(+height, +width);
     const resizedImage = await sharpImage
       .resize(width, height)
